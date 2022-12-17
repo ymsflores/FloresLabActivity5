832a59478b94f59bd14fdc5c8f563b7003e78137
ymsflores <yureenflores@gmail.com>
2022-12-17 19:46:07 +0800

    Add SimpleCalc main function

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,5 +1,7 @@
 package SimpleCalc;
 
+import FoodOrdering.FoodOrderGUI;
+
 import javax.swing.*;
 
 public class SimpleCalcGUI extends JFrame {
@@ -9,5 +11,17 @@ public class SimpleCalcGUI extends JFrame {
     private JButton btnCompute;
     private JTextField tfNumber2;
     private JTextField lblResult;
+
+    public SimpleCalcGUI() {
+
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Food Ordering System");
+        frame.setContentPane(new SimpleCalcGUI().panel1);
+        frame.setSize(500, 700);
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 
 }
