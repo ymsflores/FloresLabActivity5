bc2433eb760fd411f1057e28e283d06db4e91810
ymsflores <yureenflores@gmail.com>
2022-12-17 21:44:22 +0800

    Complete and add exception handling to SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -3,6 +3,11 @@ package SimpleCalc;
 import FoodOrdering.FoodOrderGUI;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 
 public class SimpleCalcGUI extends JFrame {
     private JPanel panel1;
@@ -12,16 +17,54 @@ public class SimpleCalcGUI extends JFrame {
     private JTextField tfNumber2;
     private JTextField lblResult;
 
-    public SimpleCalcGUI() {
+    DecimalFormatSymbols otherSymbols = new DecimalFormatSymbols(Locale.US);
+    DecimalFormat df = new DecimalFormat("#.#####", otherSymbols);
 
+    public SimpleCalcGUI() {
+        lblResult.setEditable(false);
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String op = cbOperations.getSelectedItem().toString();
+                try {
+                    float x = Float.parseFloat(tfNumber1.getText());
+                    float y = Float.parseFloat(tfNumber2.getText());
+                    float result = 0;
+                    switch (op) {
+                        case "+":
+                            result = x + y;
+                            break;
+                        case "-":
+                            result = x - y;
+                            break;
+                        case "*":
+                            result = x * y;
+                            break;
+                        case "/":
+                            if (y == 0) {
+                                throw new ArithmeticException();
+                            }
+                            result = x / y;
+                            break;
+                    }
+
+                    lblResult.setText(df.format(result));
+                } catch (NumberFormatException e1) {
+                    JOptionPane.showMessageDialog(null, "Please enter a valid number in both fields.");
+                } catch (ArithmeticException e1) {
+                    JOptionPane.showMessageDialog(null, "Cannot divide by 0");
+                }
+            }
+        });
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Food Ordering System");
+        JFrame frame = new JFrame("Simple Calculator");
         frame.setContentPane(new SimpleCalcGUI().panel1);
         frame.setSize(500, 700);
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
+
 
 }
